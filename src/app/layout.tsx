2f41370b0d945a0575740e415473326bb99d3596
BALA SANKAR <balasankarveeramani@gmail.com>
2025-12-29 04:56:15 +0000

    fix this for all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       >
         <div className="relative flex min-h-screen flex-col">
           <Header />
-          <main className="flex-1">{children}</main>
+          <main className="flex-1 pt-24">{children}</main>
         </div>
         <Toaster />
       </body>
