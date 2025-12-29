f2b621d9d04ad7da366fca41f84f747517dc060a
yrrr83632 <yrrr83632@gmail.com>
2025-12-29 11:15:13 +0530

    Create next.config.js

---
+++ .github/workflows/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/yrrr83632',
+  assetPrefix: '/yrrr83632/',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
